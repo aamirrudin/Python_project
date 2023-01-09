c613298c0809356a848304ad2e8252a3950e5f27
aamirrudin <116907675+aamirrudin@users.noreply.github.com>
2023-01-10 00:42:10 +0800

    Update trigonometric_calculator.py

--- trigonometric_calculator.py
+++ trigonometric_calculator.py
@@ -2,20 +2,20 @@
 def calc():
     user = input(str("State your trigonometric identities?"))
 
-    if user == input("sine"):
-        b = float(input("Enter value b"))
-        c = float(input("Enter value c"))
-        return sine_O
-    elif user == input("cos"):
-        a = float(input("Enter value a"))
-        c = float(input("Enter value c"))
-        return cos_O
-    elif user == input("tan"):
-        b = float(input("Enter value b"))
-        a = float(input("Enter value a"))
-        return tan_O
-    else:
-        return "Invalid request"
+    if user == "sine":
+        b = float(input("Enter value b: "))
+        c = float(input("Enter value c: "))
+        return sine_O (b,c)
+    elif user == "cos":
+        a = float(input("Enter value a: "))
+        c = float(input("Enter value c: "))
+        return cos_O (a,c)
+    elif user == "tan":
+        b = float(input("Enter value b: "))
+        a = float(input("Enter value a: "))
+        return tan_O (b,a)
+    
+    return "Invalid request"
 
 
 # setting the trigonometric identities formulaic
@@ -28,3 +28,5 @@ def cos_O (a,c):
 
 def tan_O (b,a):
     return float(b/a)
+
+print(calc())
